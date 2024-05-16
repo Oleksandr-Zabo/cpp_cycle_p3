e7b7b88f310b60d441ff16d786c6354775f9495c
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-16 19:00:36 +0300

    ex.1 lad had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
-//
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int start_range, end_range, num, temp;
+	cout << "Enter start of range: ";
+	cin >> start_range;
+	cout << "Enter start of range: ";
+	cin >> end_range;
+
+	if (start_range > end_range)
+	{
+		temp = start_range;
+		start_range = end_range;
+		end_range = temp;
+	}
+
+	do 
+	{
+		cout << "Enter number (in your range): ";
+		cin >> num;
+		if (num < start_range || num > end_range) {
+			cout << "Incorect data" << endl;
+		}
+	} while (num < start_range || num > end_range);
+
+	cout << endl;
 	system("pause");
 	return 0;
 }
